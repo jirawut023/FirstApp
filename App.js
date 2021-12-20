81cb0c56375027901f59129f45f142db9371b82f
jirawut023 <jirawut.314503@gmail.com>
2021-12-20 17:39:23 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { bounce } from 'react-native/Libraries/Animated/Easing';
+
+export default function App() {
+    return ( 
+        <View style = { styles.container } >
+        <Text style = { styles.textbold} > Mr.Jirawut Siangsanoh </Text> 
+        <Text style = { styles.textbluebold} >Mr.Jirawut Siangsanoh  </Text> 
+        <StatusBar style = "auto"/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'skyblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textbold: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'red',
+    },
+    textbluebold:{
+        fontSize: 15,
+        fontWeight:'bold',
+        color:'blue'
+    }
+});
